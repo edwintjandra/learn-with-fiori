9b9e2c694c7edabd52cb157b76aec3a021bd9afd
Edwin Tjandraatmadja <edwin.tjandraatmadja@binus.edu>
2024-06-13 22:20:17 +0700

    init laravel with tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold text-gray-800 mb-4">Tailwind CSS Testing</h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+        Primary Button
+    </button>
+</body>
+</html>
