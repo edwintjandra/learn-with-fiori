230f56abf6ed0d218109fbe1031518666a0b15b4
gamadigdojo <gamadigdojo@gmail.com>
2024-06-14 11:11:30 +0700

    add sign in

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -19,12 +19,7 @@
 <section class="bg-gradient-to-r from-[#EEF2F5] to-[#E7F5FF]">
     <form class="flex flex-col gap-2 justify-center items-center h-screen w-screen" action="">
         <img src="{{ asset('assets/Fiori.svg') }}" alt="">
-        <h1 class="font-nunito text-3xl font-medium text-custom-black">Sign up</h1>
-        <div class="flex flex-col gap-1">
-            <label for="name" class="block font-nunito text-base text-custom-black">Name</label>
-            <input type="text" id="name" class="bg-custom-bg-input border border-custom-grey pl-2 pr-[16px] py-[8px] rounded-md text-custom-black text-sm w-[360px] placeholder:text-custom-blue" placeholder="Enter your name"/>
-            <p class="text-custom-red font-nunito text-sm hidden" id="nameError">Name Invalid!</p>
-        </div> 
+        <h1 class="font-nunito text-3xl font-medium text-custom-black">Sign in</h1>
         <div class="flex flex-col gap-1">
             <label for="email" class="block font-nunito text-base text-custom-black">Email</label>
             <input type="email" id="email" class="bg-custom-bg-input border border-custom-grey pl-2 pr-[16px] py-[8px] rounded-md text-custom-black text-sm w-[360px] placeholder:text-custom-blue" placeholder="Enter your email"/>
@@ -40,24 +35,14 @@
             </div>
             <p class="text-custom-red font-nunito text-sm hidden" id="passwordError">Password Invalid!</p>
         </div> 
-        <div class="flex flex-col gap-1">
-            <label for="cpassword" class="block font-nunito text-base text-custom-black">Confirm Password</label>
-            <div class="relative">
-               <input type="password" id="cpassword" class="bg-custom-bg-input border border-custom-grey pl-2 pr-[16px] py-[8px] rounded-md text-custom-black text-sm w-[360px] placeholder:text-custom-blue" placeholder="Enter your password">
-               <button type="button" id="ctogglePassword" class="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none">
-                   <img id="cpasswordIcon" src="/assets/eye-close.svg" class="h-5 w-5 text-gray-500">
-               </button>
-            </div>
-            <p class="text-custom-red font-nunito text-sm hidden" id="cpasswordError">Password Not Match!</p>
-        </div> 
         <button id="continueButton" class="mt-2 w-[360px] py-[8px] flex justify-center font-nunito text-base text-custom-white bg-custom-blue rounded-md hover:bg-custom-hover disabled:opacity-50 disabled:cursor-not-allowed" style="box-shadow: 2px 4px 12px 0px rgba(102, 194, 255, 0.24);" disabled>
             Continue
         </button>
-        <p class="font-nunito text-base text-custom-black font-normal">Already have an account? <span class="font-bold hover:text-custom-hover"><a href=""> Sign in</a></span></p>
+        <p class="font-nunito text-base text-custom-black font-normal">Don't have an account? <span class="font-bold hover:text-custom-hover"><a href=""> Sign up</a></span></p>
     </form>
 
 </section>
 
-<script src="{{ asset('js/signup.js') }}"></script>    
+<script src="{{ asset('js/signin.js') }}"></script>    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('signup');
+});
+
+Route::get('/i', function () {
     return view('signin');
 });
 
