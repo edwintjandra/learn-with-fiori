d3879c20505f7839d98afeffab670e4551d993a0
Edwin Tjandraatmadja <edwin.tjandraatmadja@binus.edu>
2024-06-15 10:27:55 +0700

    Remove user signup, add admin account seeder

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FioriController;
 
-Route::get('/', [UserController::class, 'index']);
-Route::post('/', [UserController::class, 'signup'])->name('signup');
-Route::get('/signin', [UserController::class, 'signinPage'])->name('signinPage');
+Route::get('/', [UserController::class, 'signinPage'])->name('signinPage');
 Route::post('/signin', [UserController::class, 'signin'])->name('signin');
-Route::middleware('auth')->post('/profile', [UserController::class, 'updateProfile'])->name('updateProfile');
+ 
+Route::middleware(['auth'])->group(function(){
+    Route::post('/profile', [UserController::class, 'updateProfile'])->name('updateProfile');
+
+});
 
 Route::get('/fiori',[FioriController::class,'index'])->name('fiori.index');
 Route::post('/fiori',[FioriController::class,'handleMessage'])->name('fiori.handle.message');
