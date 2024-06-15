79b159a84e7673b4c781741fc3954411275ef0ef
Edwin Tjandraatmadja <edwin.tjandraatmadja@binus.edu>
2024-06-15 13:51:57 +0700

    Prompt and Response summarizer done

--- resources/views/fiori/index.blade.php
+++ resources/views/fiori/index.blade.php
@@ -32,7 +32,7 @@
                 <!-- fiori response -->
                 <div class="response flex gap-2 items-start mb-5">
                     <img src="{{ asset('assets/Fiori.svg') }}" alt="" class="w-10">
-                    <p class="text-left font-nunito text-base text-wrap">Lorem, ipsum dolor.</p>
+                    <p class="response-text text-left font-nunito text-base text-wrap">Lorem, ipsum dolor.</p>
                 </div>
 
                 <!-- customer questions -->
@@ -49,7 +49,7 @@
                 <div class="response flex gap-2 mb-5 items-start">
                     <img src="{{ asset('assets/Fiori.svg') }}" alt="" class="w-10">
                     <div class="">
-                        <p class="text-left font-nunito text-base text-wrap mb-2">Lorem ipsum dolor sit amet,
+                        <p class="response-text text-left font-nunito text-base text-wrap mb-2">Lorem ipsum dolor sit amet,
                             consectetur adipisicing elit. Magnam rem voluptas incidunt laudantium! Error excepturi sed
                             repellendus corporis exercitationem ut ipsam. Nisi facere neque officiis voluptatum commodi.
                             Quia, a fugit! Tenetur minima distinctio ipsum alias sunt, dignissimos commodi excepturi
@@ -68,26 +68,29 @@
                                                     d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
                                                 </path>
                                             </svg>
-                                            <span class="text-custom-blue font-nunito text-base font-medium">Question summarized</span>
+                                            <span class="text-custom-blue font-nunito text-base font-medium">Summary</span>
                                         </summary>
 
                                         <article class="px-4 pb-4 text-sm">
                                             <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et ipsum
-                                                sapien. Vestibulum molestie
-                                                porttitor augue vitae vulputate. Aliquam nec ex maximus, suscipit diam
-                                                vel, tristique tellus. Lorem ipsum dolor sit amet consectetur
-                                                adipisicing elit. Ipsa officiis blanditiis placeat repudiandae eum ab
-                                                modi itaque dignissimos ex perferendis. Officia dolores obcaecati magnam
-                                                deleniti quod incidunt repellat odio. Itaque illum animi doloribus,
-                                                nihil temporibus sunt. Ea, a, nesciunt alias quidem, aliquam aspernatur
-                                                dolorum nulla delectus recusandae explicabo nam minus perspiciatis sed.
-                                                Delectus excepturi quam magni quo laboriosam minima consequuntur iste a
-                                                suscipit et ea vitae enim molestias tempora, nostrum eveniet animi,
-                                                reprehenderit non earum obcaecati. Unde, ab id. Numquam perspiciatis
-                                                ipsam at obcaecati vero velit est eius praesentium ipsa debitis
-                                                repellendus eos necessitatibus soluta suscipit, architecto repudiandae
-                                                iusto optio!
+                                                <ul class="list-disc">
+                                                    Prompt Summary
+                                                    <li>Numquam perspiciatis ipsam at obcaecati vero velit est eius praesentium ipsa debitis repellendus eos necessitatibus soluta suscipit, architecto repudiandae iusto optio!</li>
+                                                    <li>Ipsa officiis blanditiis placeat repudiandae eum ab modi itaque dignissimos ex perferendis.
+                                                    </li>
+                                                    <li>Delectus excepturi quam magni quo laboriosam minima consequuntur iste a suscipit et ea vitae enim molestias tempora, nostrum eveniet animi, reprehenderit non earum obcaecati.
+                                                    </li>
+
+                                                    <br>
+
+                                                    Response Summary
+                                                    <li>Numquam perspiciatis ipsam at obcaecati vero velit est eius praesentium ipsa debitis repellendus eos necessitatibus soluta suscipit, architecto repudiandae iusto optio!</li>
+                                                    <li>Ipsa officiis blanditiis placeat repudiandae eum ab modi itaque dignissimos ex perferendis.
+                                                    </li>
+                                                    <li>Delectus excepturi quam magni quo laboriosam minima consequuntur iste a suscipit et ea vitae enim molestias tempora, nostrum eveniet animi, reprehenderit non earum obcaecati.
+                                                    </li>
+                                                </ul>
+                                               
                                             </p>
                                         </article>
                                     </details>
@@ -154,6 +157,9 @@
     </section>
 
     <script src="{{ asset('js/fioriopenai.js') }}"></script>
+    <script src="{{ asset('js/js-summarize/lib/lodash/lodash.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/js-summarize/lib/tokenizer/tokenizer.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/js-summarize/js-summarize.js') }}" type="text/javascript"></script>
 
 
     <script>
@@ -175,7 +181,37 @@
                         "prompt": $("#prompt").val()
                     }
                 }).done(function(res) {
+                    //javascript summarizer class
+                    var summarizer = new JsSummarize({
+                        returnCount: 5 // Request a summary of 3 sentences
+                    });
+
+                    //prompt summary
+                    var promptSummaryArray = summarizer.summarize("prompt summarize",promptValue);
+                    var ulPromptSummary = '<ul class="list-disc">';
+                    ulPromptSummary+='Prompt Summary';
+                    // Summarize the response
+                    promptSummaryArray.forEach(function(summary) {
+                        ulPromptSummary += '<li>' + summary + '</li>';
+                    });
+
+                    ulPromptSummary+='</ul> </br>'; 
+
+                    //response summary
+                    var responseSummaryArray = summarizer.summarize("response summarizer",res);
+                    // Summarize the response
+                    var ulResponseSummary = '<ul class="list-disc">';
+                    ulResponseSummary+='Response Summary';
+
+                    responseSummaryArray.forEach(function(summary) {
+                        ulResponseSummary += '<li>' + summary + '</li>';
+                    });
+
+                    ulResponseSummary += '</ul>'
+                   
+                   
                     
+                                        
                     //populate sending prompt
                     var newPrompt =
                         '<div class="prompt flex gap-0 items-start mb-5">' +
@@ -192,7 +228,7 @@
                         '<div class="response flex gap-2 mb-5 items-start">' +
                         '<img src="{{ asset('assets/Fiori.svg') }}" alt="" class="w-10">' +
                         '<div class="">' +
-                        '<p class="text-left font-nunito text-base text-wrap mb-2">' +
+                        '<p class="response-text text-left font-nunito text-base text-wrap mb-2">' +
                         res+
                         '</p>' +
                         '<div class="dropdown">' +
@@ -203,11 +239,12 @@
                         '<svg class="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="fill-[#C9CCCE]" viewBox="0 0 16 16">' +
                         '<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>' +
                         '</svg>' +
-                        '<span class="text-custom-blue font-nunito text-base font-medium">Question summarized</span>' +
+                        '<span class="text-custom-blue font-nunito text-base font-medium">Summary</span>' +
                         '</summary>' +
                         '<article class="px-4 pb-4 text-sm">' +
                         '<p>' +
-                        'testadsadsa'+
+                        ulPromptSummary+
+                        ulResponseSummary+
                         '</p>' +
                         '</article>' +
                         '</details>' +
